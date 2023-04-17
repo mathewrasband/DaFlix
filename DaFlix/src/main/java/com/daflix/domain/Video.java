1b6ca68f6da53c6b8e830cd37479c4233734dda1
mathewrasband <58843063+mathewrasband@users.noreply.github.com>
2023-04-17 11:21:32 -0600

    Add setters and getters
    Added setters and getters to Videos

--- DaFlix/src/main/java/com/daflix/domain/Video.java
+++ DaFlix/src/main/java/com/daflix/domain/Video.java
@@ -28,4 +28,28 @@ public class Video {
 		this.name = name;
 		this.data = data;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public byte[] getData() {
+		return data;
+	}
+
+	public void setData(byte[] data) {
+		this.data = data;
+	}
 }
