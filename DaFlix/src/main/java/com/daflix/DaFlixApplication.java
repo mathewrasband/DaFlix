f4abf7a6143f0b63d4b369631bdd8af4ca41e2d0
mathewrasband <58843063+mathewrasband@users.noreply.github.com>
2023-04-17 10:04:52 -0600

    Initial Commit

---
+++ DaFlix/src/main/java/com/daflix/DaFlixApplication.java
@@ -0,0 +1,13 @@
+package com.daflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaFlixApplication.class, args);
+	}
+
+}
---
+++ DaFlix/src/test/java/com/daflix/DaFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
