69c82aebc468ba01040e8df3feed9f6a21973485
mathewrasband <58843063+mathewrasband@users.noreply.github.com>
2023-04-17 11:22:07 -0600

    Update VideoController.java
    edited getVideoByName to have it work in Chrome.

--- DaFlix/src/main/java/com/daflix/controllers/VideoController.java
+++ DaFlix/src/main/java/com/daflix/controllers/VideoController.java
@@ -3,14 +3,21 @@ package com.daflix.controllers;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.daflix.exceptions.VideoNotFoundException;
 import com.daflix.services.VideoService;
 
 import lombok.AllArgsConstructor;
@@ -29,6 +36,14 @@ public class VideoController {
 	}
 	
 	@GetMapping("{name}")
+	public ResponseEntity<Resource> getVideoByName(@PathVariable("name") String name) throws VideoNotFoundException{
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.contentType(MediaType.APPLICATION_OCTET_STREAM)
+				.body(new ByteArrayResource(videoService.getVideo(name).getData()));
+	}
+	
+	@GetMapping("all")
 	public ResponseEntity<List<String>> getAllVideoNames(){
 		return ResponseEntity.ok(videoService.getAllVideoNames());
 	}
